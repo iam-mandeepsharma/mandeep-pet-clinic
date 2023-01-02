8a241660c6f959fa1f9e229d064e3b13b4ff1136
Mandeep Sharma <manu_prashar@hotmail.com>
2023-01-02 20:28:48 +0530

    Initial project initialiszed

---
+++ src/main/java/mandeep/springlearning/mandeeppetclinic/MandeepPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mandeep.springlearning.mandeeppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandeepPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandeepPetClinicApplication.class, args);
+	}
+
+}
