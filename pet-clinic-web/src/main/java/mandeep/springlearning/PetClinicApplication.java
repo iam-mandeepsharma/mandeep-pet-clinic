4b257e5d9842373489abc5b6a5a44a772ecd34ad
Mandeep Sharma <manu_prashar@hotmail.com>
2023-01-21 19:23:50 +0530

    Main class

--- pet-clinic-web/src/main/java/mandeep/springlearning/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/mandeep/springlearning/PetClinicApplication.java
@@ -1,4 +1,12 @@
 package mandeep.springlearning;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class);
+    }
 }
